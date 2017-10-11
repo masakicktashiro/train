770e0c1d4fa60da3eaa514d96de3bd4d02b847df
masakick07 <masaki.sfc@gmail.com>
2017-10-12 01:20:43 +0900

    追加しました

---
+++ nlp2.py
@@ -0,0 +1,6 @@
+a="パタトクカシーー"
+b=""
+for i,p in enumerate(a):
+    if i%2==1:
+        b+=p
+print(b)
---
+++ nlp33.py
@@ -0,0 +1,3 @@
+a="Now I need a drink, alcoholic of course, after the heavy involving quantum mechanics."
+b=[len(p) for p in a.split()]
+print(b)
